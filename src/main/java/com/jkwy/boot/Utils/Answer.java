749314327a58a60637d0cfb712c6972bd967c755
wangxinghao <ai141500@163.com>
2018-05-15 13:27:49 +0800

    boot1.0

--- src/main/java/com/jkwy/boot/Utils/Answer.java
+++ src/main/java/com/jkwy/boot/Utils/Answer.java
@@ -1,13 +1,21 @@
 package com.jkwy.boot.Utils;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 /**
  * Created by wangxinghao on 2018/5/15.
  */
 public class Answer {
 
-    Document document = Jsoup.connect(URL)
-            .userAgent(
-                    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31")
-            .timeout(10000).get();
+    public static void mian()throws IOException{
+        Document document = Jsoup.connect("")
+                .userAgent(
+                        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31")
+                .timeout(10000).get();
+    }
+
 
 }
